248fb2ca8d46b53cfb553f9a68b7d9e50335f0c6
caotsinghua <1577499543@qq.com>
2017-11-05 02:47:36 +0800

    add cors

--- app.js
+++ app.js
@@ -1,15 +1,22 @@
-const Koa=require('koa')
-const BodyParser=require('koa-bodyparser');
-const indexRouter=require('./routers/index')
-const app=new Koa();
+const Koa = require('koa')
+const BodyParser = require('koa-bodyparser');
+const cors = require('koa2-cors');
+const indexRouter = require('./routers/index')
+
+const app = new Koa();
+app.use(cors({
+	origin: function(ctx) {
+		return '*';
+	}
+}));
 app.use(BodyParser());
 app.use(indexRouter.routes()).use(indexRouter.allowedMethods());
 
-app.listen('8083',(e)=>{
-  if(e){
-    console.error(e.message)
-  }else{
-    console.log('[bookCrawlerServer] listen on 8083');
-  }
+app.listen('8083', (e) => {
+	if (e) {
+		console.error(e.message)
+	} else {
+		console.log('[bookCrawlerServer] listen on 8083');
+	}
 
 })
